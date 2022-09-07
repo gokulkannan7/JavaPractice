e5a5e2231735e95fe5611dbc4edafa6826cee78e
gokulkannan7 <gokulrao31@gmail.com>
2022-09-07 15:16:28 +0530

    Method practice

--- src/Hello.java
+++ src/Hello.java
@@ -1,3 +1,35 @@
 public class Hello {
 
+    public static void main(String args[]){
+        System.out.println("Hello World!!");
+        int highScore = 50;
+        double kilometers = (100 * 1.69);
+        if(highScore==50){
+            System.out.println("Wow bro impressed!");
+        }
+        //calculateScore();
+
+    }
+
+    public static void calculateScore(){
+        System.out.println("This is from the calculateScore method");
+    }
+
+    public static void displayHighScorePosition(String name, int pos){
+        System.out.println(name + " player has scored" + pos + " position");
+
+    }
+
+    public static int calculateHighScorePosition(int score){
+        if(score>1000){
+            return 1;
+        }
+        else if(score>500 && score<1000){
+            return 2;
+        }
+        else if(score>100 && score<500){
+            return 3;
+        }
+        else return 4;
+    }
 }
